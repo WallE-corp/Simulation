4464da0a9c7dfc449a50b8316a64a84fcfec28b2
tifufu <dematasjoshua@hotmail.com>
2022-05-08 23:22:21 +0200

    .

---
+++ UnitySimulation/Assets/Scripts/MovementCommand.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct MovementCommand
+{
+    public int type;
+    public string direction;
+    public string action;
+}
+
---
+++ UnitySimulation/Assets/Scripts/PositionData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PositionData : MessageData
+{
+    int type = 1;
+    public double x;
+    public double y;
+}
