343e045b6ee7d844a2f60955bcb1939c88284fae
tifufu <dematasjoshua@hotmail.com>
2022-05-09 02:29:56 +0200

    Form design for MobileMock

--- MobileMock/MobileMock/MobileMock/Form1.cs
+++ MobileMock/MobileMock/MobileMock/Form1.cs
@@ -5,6 +5,13 @@ namespace MobileMock
         public mainForm()
         {
             InitializeComponent();
+            tbxServerAddress.Text = "127.0.0.1:3000";
+            gbxControls.Enabled = false;
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
